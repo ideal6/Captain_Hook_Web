28a855577b97fa40510a0114a4d5abaada1385a1
KANCHOEUN <kancho1216@naver.com>
2021-05-18 15:53:09 +0000

    refactor: input component

--- components/atoms/input/index.tsx
+++ components/atoms/input/index.tsx
@@ -1,17 +1,11 @@
 import cn from 'classnames'
 
-interface DataList {
-  id: string
-  options: Array<string>
-}
-
 interface InputProps {
   type: 'email' | 'password' | 'radio' | 'checkbox' | 'search' | 'text' | 'url'
   id: string
   name: string
   size: 'auth' | 'small' | 'normal' | 'big'
   placeholder?: string | undefined
-  list?: DataList | undefined
 }
 
 const inputSize = {
@@ -21,45 +15,22 @@ const inputSize = {
   big: 'w-480 h-10',
 }
 
-const Input: React.FC<InputProps> = ({
-  type,
-  id,
-  name,
-  placeholder,
-  list,
-  size,
-}) => {
+const Input: React.FC<InputProps> = ({ type, id, name, size, placeholder }) => {
   return (
-    <>
-      <input
-        type={type}
-        id={id}
-        name={name}
-        placeholder={placeholder}
-        list={list?.id}
-        className={cn(
-          'rounded',
-          'pl-3',
-          'pr-2',
-          'text-base',
-          'text-gray-300',
-          'border',
-          'border-gray-300',
-          'focus:outline-none',
-          'focus:border-opacity-0',
-          'focus:ring-2',
-          'focus:ring-primary',
-          inputSize[size]
-        )}
-      />
-      {list && (
-        <datalist id={list.id}>
-          {list.options.map((option, idx) => {
-            return <option value={option} key={idx}></option>
-          })}
-        </datalist>
+    <input
+      type={type}
+      id={id}
+      name={name}
+      placeholder={placeholder}
+      className={cn(
+        'rounded',
+        'pl-3 pr-2',
+        'text-base text-gray-300',
+        'border border-gray-300',
+        'focus:outline-none focus:border-opacity-0 focus:ring-2 focus:ring-primary',
+        inputSize[size]
       )}
-    </>
+    />
   )
 }
 
