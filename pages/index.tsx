04a3f231dfec1b9eb004aaaec7177ef3feb03842
wjdtmddnr24 <wjdtmddnr24@naver.com>
2021-06-24 16:56:43 +0000

    fix: Merge Conflict

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,49 @@
 import cn from 'classnames'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { getApiClient } from '../utils/getApiClient'
 
-const Home: React.FC = () => (
-  <div className={cn('w-full h-full')}>
-    <Head>
-      <title>Hook선장</title>
-    </Head>
-    <main>
-      {' '}
-      <h1>페이지</h1>
-      <ul>
-        <li>
-          <Link href="/webhooks">
-            <a>Webhook List Page</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/notifications">
-            <a>Notification List Page</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/login">
-            <a>Login Page</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/signup">
-            <a>Sign Up Page</a>
-          </Link>
-        </li>
-      </ul>
-    </main>
-  </div>
-)
+const Home: React.FC = () => {
+  const apiClient = getApiClient()
+  const [user, setUser] = useState(undefined)
+  useEffect(() => {
+    apiClient.get('/users/me').then(({ data }) => setUser(data))
+  }, [])
+  return (
+    <div className={cn('w-full h-full')}>
+      <Head>
+        <title>Hook선장</title>
+      </Head>
+      <main>
+        {' '}
+        <span>User: {user && user.username}</span>
+        <h1>페이지</h1>
+        <ul>
+          <li>
+            <Link href="/webhooks">
+              <a>Webhook List Page</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/notifications">
+              <a>Notification List Page</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/login">
+              <a>Login Page</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/signup">
+              <a>Sign Up Page</a>
+            </Link>
+          </li>
+        </ul>
+      </main>
+    </div>
+  )
+}
 
 export default Home
