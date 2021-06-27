567a65b87bce9b0928012b26e68d853fd705cc63
wjdtmddnr24 <wjdtmddnr24@naver.com>
2021-06-27 14:45:32 +0000

    fix: Td Component

--- components/atoms/td/index.tsx
+++ components/atoms/td/index.tsx
@@ -4,19 +4,25 @@ interface TdProps {
   // eslint-disable-next-line @typescript-eslint/ban-types
   record: any
   onClickHandler: React.MouseEventHandler<HTMLElement>
+  filter?: string[]
 }
 
-const Td: React.FC<TdProps> = ({ record, onClickHandler }) => {
+const Td: React.FC<TdProps> = ({ record, onClickHandler, filter = [] }) => {
   return (
     <>
-      {Object.entries(record).map(([_, value], idx) => (
-        <td
-          key={idx}
-          className={cn('px-6 py-4 whitespace-nowrap', 'text-gray-800 text-sm')}
-        >
-          {value}
-        </td>
-      ))}
+      {Object.entries(record)
+        .filter((r) => (filter.length === 0 ? true : filter.includes(r[0])))
+        .map(([_, value], idx) => (
+          <td
+            key={idx}
+            className={cn(
+              'px-6 py-4 whitespace-nowrap',
+              'text-gray-800 text-sm'
+            )}
+          >
+            {value}
+          </td>
+        ))}
       <td className={cn('pr-5 py-3 whitespace-nowrap', 'text-sm font-medium')}>
         <span
           onClick={onClickHandler}
