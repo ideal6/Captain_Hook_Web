a062a55bd24ee31f341ddb1b4ee129f421a24a9f
wjdtmddnr24 <wjdtmddnr24@naver.com>
2021-06-27 12:42:15 +0000

    feat: WebhookModify Page

---
+++ pages/webhooks/[id].tsx
@@ -0,0 +1,20 @@
+import cn from 'classnames'
+import AppBar from './../../components/organisms/appbar'
+import SideBar from './../../components/organisms/sidebar'
+import WebhookModify from './../../components/organisms/webhookmodify'
+
+const Modify: React.FC = () => {
+  return (
+    <div className={cn('flex flex-col h-screen')}>
+      <AppBar />
+      <div className={cn('flex flex-row flex-grow bg-gray-100')}>
+        <SideBar />
+        <div className={cn('flex w-full')}>
+          <WebhookModify />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modify
